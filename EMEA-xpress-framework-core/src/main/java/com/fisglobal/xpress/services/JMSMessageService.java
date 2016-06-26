9efdd739b5f90722c518e330525aba2008386b50
Mukul Agrawal <mukulagrawal@Mukuls-MacBook-Pro-397.local>
2016-06-26 19:35:30 +0200

    deleting the obnoxious

--- EMEA-xpress-framework-core/src/main/java/com/fisglobal/xpress/services/JMSMessageService.java
+++
@@ -1,9 +0,0 @@
-package com.fisglobal.xpress.services;
-
-
-import com.fnf.xes.framework.ServiceException;
-
-public interface JMSMessageService {
-    Object processMessage(Object request) throws ServiceException;
-    boolean accept(Object request) throws ServiceException;
-}
